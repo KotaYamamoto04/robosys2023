322a9b6a3552e5028f528a3ba7e5a9a6473c7f10
Kota Yamamoto <s22c1133uz@s.chibakoudai.jp>
2023-11-28 18:14:20 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -9,18 +9,18 @@ ng () {
 
 res=0
 
-  ### I/O ###
-  out=$(seq 5 | ./plus)
-  [ "${out}" = 15 ] || ng ${LINENO}
+### I/O ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
     　 
-  ### STRANGE INPUT ###
-  out=$(echo あ | ./plus)
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
     　 
-  out=$(echo | ./plus) #空文字
-  [ "$?" = 1 ]      || ng ${LINENO}
-  [ "${out}" = "" ] || ng ${LINENO}
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
     　 
 [ "$res" = 0 ] && echo OK
-  exit $res
+exit $res
